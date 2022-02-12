1ee0d629800f6b317eb7e6c224cd53dbc777fe91
StephanieHochgeschurz <stephanie@hochgeschurz.com>
2022-02-12 10:53:59 +0100

    Documentation was added to the UserNameNotExisting exception.

--- exceptions.py
+++ exceptions.py
@@ -1,22 +1,15 @@
 class UserNameNotExisting(Exception):
-    """Raised when user tries to create a user name that is already existing"""
+    """Indicates that a user with the entered user name does not exist.
+    Is raised when a user tries to login with a username that is not stored in the database, i.e., unused.
 
-    def __init__(self, username,
-                 message="A user with this username does not exist."):
+    Attributes:
+        username ('str'): the entered username that does not exist
+        message ('str'): the error message that can be displayed
+    """
+
+    def __init__(self, username):
         self.username = username
-        self.message = message
-        super().__init__(self.message)
-
-    def __str__(self):
-        return f'{self.message}'
-
-
-class UserHasNoHabits(Exception):
-    """Raised when user tries to do something with habits, but does not have any habits yet"""
-    def __init__(self, username,
-                 message="You do not have any habits yet. To perform this action, you first need to create habits."):
-        self.username = username
-        self.message = message
+        self.message = "A user with this username does not exist."
         super().__init__(self.message)
 
     def __str__(self):
